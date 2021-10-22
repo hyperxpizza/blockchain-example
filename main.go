4ba0183fc994c44ead59222c787cb3a3a17e29c3
hyperxpizza <wojtek.frackowski@gmail.com>
2021-10-22 19:50:00 +0200

    makefile and binary

--- main.go
+++ main.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"log"
+	"os"
 
 	"github.com/hyperxpizza/blockchain-example/blockchain"
 	"github.com/hyperxpizza/blockchain-example/cli"
 )
 
 func main() {
+	defer os.Exit(1)
 	chain, err := blockchain.InitBlockChain()
 	if err != nil {
 		log.Fatal(err)
